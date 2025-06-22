4723fe5be2954a8eca10eeb2ba3e21ee9d150bae
shreyash gote <goteshreyash50@gmail.com>
2025-06-22 11:50:05 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import {
+  Hero,
+  CustomerReviews,
+  Services,
+  SpecialOffers,
+  PopularProduct,
+  SuperQuality,
+  Footer,
+  Subscribe,
+} from "./section";
+import Nav from "./Components/Nav";
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProduct />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+export default App;
---
+++ src/section/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  Footer,
+  PopularProduct,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews,
+};
